3b43eec74f68bf3c2a22154fb15a41164566016f
paiman <paimanrasoli789@gmail.com>
2021-05-16 19:30:37 +0430

    add functionality and animation for heading and catagory part

--- components/Header.js
+++ components/Header.js
@@ -1,71 +1,73 @@
-import {useEffect, useState} from 'react'
-import axios from 'axios';
-import {API_URL} from '../config/index'
-import Link from 'next/link'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "../config/index";
+import Skeleton from "react-loading-skeleton";
+import Link from "next/link";
 
 export default function Header() {
-
-  const [load, setLoad] = useState(false)
-  const [data, setData] = useState(null)
+  const [load, setLoad] = useState(false);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    axios.get(`${API_URL}/categories`).then((res) =>{
-      setData(res.data)
-      setLoad(true)
-    })
-  },[])
+    axios.get(`${API_URL}/catagories`).then((res) => {
+      setData(res.data);
+      setLoad(true);
+    });
+  }, []);
 
-
-  if(load){
+  if (load) {
     return (
-
-      <nav
-        className="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid                   border-t-2 border-blue-700">
+      <nav className="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid                   border-t-2 border-blue-700">
         <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2                   border-gray-300 pb-5 lg:pb-0">
-            <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
-                <span className="font-semibold text-xl tracking-tight">My Navbar</span>
-            </div>
-            <div className="block lg:hidden ">
-                <button
-                    id="nav"
-                    className="flex items-center px-3 py-2 border-2 rounded text-blue-700 border-blue-700 hover:text-blue-700 hover:border-blue-700">
-                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
-                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
-                    </svg>
-                </button>
-            </div>
+          <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
+            <span className="font-semibold text-xl tracking-tight">News</span>
+          </div>
+          <div className="block lg:hidden ">
+            <button
+              id="nav"
+              className="flex items-center px-3 py-2 border-2 rounded text-blue-700 border-blue-700 hover:text-blue-700 hover:border-blue-700"
+            >
+              <svg
+                className="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
         </div>
-  
-        <div className="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
-            <div className="text-md font-bold text-blue-700 lg:flex-grow">
-                {data.map((data)=> (
-                  <Link href={`/category/${data.name}`}
-                  key={`${data.id}`}
-                  >
-                      <a
-                        className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
-                          {data.name}
-                      </a>
-                  </Link>
-                ))}
-                
-            </div> 
-            <div className="flex ">
-                <a href="#"
-                  className="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Sign
-                    in</a>
-  
-                <a href="#"
-                  className=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">login</a>
-            </div>
+
+        <div className="menu w-full flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
+          <div className="text-md font-bold text-blue-700 lg:flex-grow">
+            {data.map((data) => (
+              <Link href={`/category/${data.name}`} key={`${data.id}`}>
+                <a className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
+                  {data.name}
+                </a>
+              </Link>
+            ))}
+          </div>
+          <div className="flex ">
+            <a
+              href="#"
+              className="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0"
+            >
+              Sign in
+            </a>
+
+            <a
+              href="#"
+              className=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0"
+            >
+              login
+            </a>
+          </div>
         </div>
-  
-    </nav>
-  
+      </nav>
     );
-  }else{
-    return(<>Loading</>)
+  } else {
+    return <Skeleton count={1} />;
   }
-
 }
-
--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,12 +1,48 @@
-import Layout from '../../components/Layout'
-
-const Categories = () => {
-
-    return (
-        <Layout title="Categories">
-            Categories
-        </Layout>
-    );
+import Layout from "../../components/Layout";
+import Head from "next/head";
+import Header from "../../components/Header";
+export default function Catagorys({ id, slug, needPages }) {
+  return (
+    <>
+      <Head>
+        <title>{slug}</title>
+      </Head>
+      <header>
+        <Header />
+      </header>
+      <main className="catagory">Ok</main>
+    </>
+  );
 }
- 
-export default Categories;
+
+//render selected catagory and its post
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+
+  const reqCatgoryId = await fetch(
+    `http://localhost:1337/catagories?name=${slug}`
+  );
+  const resCatagoryId = await reqCatgoryId.json();
+  //get id of catagory
+  const id = resCatagoryId[0].id;
+  const reqCount = await fetch(`http://localhost:1337/posts?catagory=${id}`);
+  const resCount = await reqCount.json();
+  const needPages = Math.ceil(resCount.length / 6);
+  return {
+    props: { id, needPages, slug },
+  };
+}
+
+// how many pages there are
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:1337/catagories");
+  const datas = await res.json();
+  const paths = datas.map((d) => ({
+    params: { slug: d.name },
+  }));
+  return {
+    paths,
+    fallback: false, //404
+  };
+}
