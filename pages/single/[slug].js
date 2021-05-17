3f6f8f768d437819c6f03f42cf2c548e019563f9
paiman <paimanrasoli789@gmail.com>
2021-05-17 13:10:02 +0430

    after pulling

---
+++ pages/single/[slug].js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+const SinglePage = () => {
+    return (
+        <Layout title="single">
+            Sinlge page
+        </Layout>
+    );
+}
+ 
+export default SinglePage;
