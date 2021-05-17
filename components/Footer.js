b6857818d7aaaedc831914dd8bf958a2b1bb2a5c
Mahdi Hassani <hassanim430@gmail.com>
2021-05-17 10:09:34 +0430

    working on header and footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,51 +1,24 @@
+import Link from 'next/link'
 export default function Footer() {
   return (
-    <>
-        <div className="flex flex-wrap justify-center bg-blue-700 p-6">
-          <div className="flex flex-wrap mb-4 w-full">
-            <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 ">
-              <h3 className="text-3xl py-4">About Us</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-              </p>
-            </div>
-            <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 pl-8">
-              <h3 className="text-3xl py-4">Links</h3>
-              <ul>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-              </ul>
-            </div>
-            <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 ">
-              <h3 className="text-3xl py-4">Links</h3>
-              <ul>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-              </ul>
-            </div>
-            <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4">
-              <h3 className="text-3xl py-4">Subscribe</h3>
-              <form action="#">
-                <div className="mb-4">
-                  
-                </div>
-                <button  className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded" type="submit">Submit</button>
-              </form>
-            </div>
-          </div>
-        </div>
-
-        <div className="bg-purple-900 p-2 pl-6">
-            <p className="bottom">© Copyright 2020 - W3hubs.com</p>
-        </div>
-    </>
+    <footer>
+       <section className="bg-bgColor w-full">
+            <ul className="container mx-auto flex justify-center py-5 text-lg uppercase">
+                <li className="px-4 cursor-pointer">
+                  <Link href="/">Home</Link>
+                </li>
+                <li className="px-4">Contact us</li>
+                <li className="px-4">About us</li>
+                <li className="px-4">Privacy Policy</li>
+                <li className="px-4">Sign in</li>
+                <li className="px-4">Sign Up</li>
+            </ul>
+       </section>
+       <section className="w-full border-t border-borderColor flex justify-center bg-title">
+            <p className="text-white py-5">
+                © Company name |  Powered by Next.js, Strapi
+            </p>
+       </section>
+    </footer>
   );
 }
