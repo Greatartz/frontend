6d87c04f4cbe97e6574050dc1edd521d5f7b836d
paiman <paimanrasoli789@gmail.com>
2021-05-24 22:12:19 +0430

    latest callback

--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,6 @@ export default function Header({ categories, load }) {
           </div>
         </div>
 
-<<<<<<< HEAD
         <div className="container mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <button
@@ -113,104 +112,6 @@ export default function Header({ categories, load }) {
           </div>
         </div>
       </nav>
-=======
-			<div className="bg-bgColor w-full">
-			  <div className="w-11/12 mx-auto">
-				  <h1 className="text-3xl uppercase py-5">
-					<Link href="/">
-						<a>
-							Mitch Cumm
-						</a>
-					</Link>
-				  </h1>
-			  </div>
-			</div>	
-
-			<div className="w-11/12 mx-auto flex flex-wrap items-center justify-between">
-				
-				<div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-					
-					<button
-						className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-						type="button"
-						onClick={() => setNavbarOpen(!navbarOpen)}
-					>
-						Menu
-					</button>
-				</div>	
-				
-				<div
-					className={
-						"lg:flex flex-grow items-left flex-col lg:flex-row" +
-						(navbarOpen ? " flex" : " hidden")
-					}
-					id="example-navbar-danger"
-				>
-					<ul className="flex flex-col lg:flex-row list-none lg:mr-auto">
-						
-						{categories.map((data) => (
-							<li className="nav-item" key={`${data.id}`}>
-								<Link href={`/category/${data.name}`}>
-									<a  category={data.id}
-										className={`${
-											router.asPath === `/category/${data.name}`
-											? "anchorActive"
-											: "anchor "
-										}`}
-									>
-										{data.name}
-									</a>
-								</Link>
-							</li>
-						))}
-						<li className="nav-item">
-							{!session && (
-								<Link href="/login">
-									<a className="anchor">
-										Sign in
-									</a>
-								</Link>
-							)}
-							{session && (
-								<a className="anchor">
-									Signed in as {session.user.username} <br />
-									<button onClick={() => signOut()}>Sign out</button>
-								</a>
-							)}
-						</li>
-					</ul>
-						
-					<div className="searchComponent">
-						<form
-							onSubmit={handleSubmit}
-							className="search-form flex bg-bgColor"
-						>
-							<button type="submit" className="search-form-link focus:outline-none">
-								<img
-								src="/search.svg"
-								alt="Search Icon"
-								className="search-icon"
-								/>
-							</button>
-							<div className="search-field-group">
-								<input
-								type="text"
-								className="pl-3 focus:outline-none bg-bgColor"
-								placeholder="Search ..."
-								value={term} 
-								onChange={ (e) => setTerm(e.target.value) }
-								title="Search for:"
-								autoComplete="off"
-								/>
-							</div>
-						</form>
-					</div>
-					
-				</div>
-				
-			</div>
-		</nav> 
->>>>>>> 8e46d5bc5246c97b596e60c213349218131032b3
     );
   } else {
     return <Skeleton count={1} />;
