4f96c9df5842d1411090a5f1084eaaac52909095
paiman <paimanrasoli789@gmail.com>
2021-05-29 16:39:40 +0430

    stable version for deploying

--- components/RichText.js
+++ components/RichText.js
@@ -5,8 +5,6 @@ export default function RichText({ doc }) {
   let md = new mark();
   md.renderer.rules.image = function (tokens, idx, options, env, slf) {
     let token = tokens[idx];
-    //should be comment for production
-    token.attrSet("src", `${API_URL}${token.attrGet("src")}`);
     token.attrPush(["id", "content_image"]);
 
     token.attrs[token.attrIndex("alt")][1] = slf.renderInlineAsText(
