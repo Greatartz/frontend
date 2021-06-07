ba61f102e830bfc4395c2a1b296f1348aecad911
paiman <paimanrasoli789@gmail.com>
2021-06-07 11:59:35 +0430

    ssr

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,31 @@
 import HomeCatagory from "../components/HomeCatagory";
 import Layout from "../components/Layout";
 import { API_URL } from "../config/index";
-import { NextSeo } from 'next-seo';
+import { NextSeo } from "next-seo";
 
 export default function Home({ resCatagory }) {
-	
-	const SEO = {
-		title: 'Page | Home',
-		description: 'MITCH CUMM Home page to show whole our website concept'
-	}	
-	
+  const SEO = {
+    title: "Page | Home",
+    description: "MITCH CUMM Home page to show whole our website concept",
+  };
+
   return (
     <Layout>
-	  <NextSeo {...SEO} />	
+      <NextSeo {...SEO} />
       <div className="mb-10">
-			{resCatagory.map((i) => (
-				<HomeCatagory category={i} key={`h-${i.id}`} />
-			))}
-	  </div>
+        {resCatagory.map((i) => (
+          <HomeCatagory category={i} key={`h-${i.id}`} />
+        ))}
+      </div>
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
+Home.getInitialProps = async () => {
   const reqCatagory = await fetch(`${API_URL}/categories`);
   const resCatagory = await reqCatagory.json();
   //check subscribtion
   return {
-    props: {
-      resCatagory,
-    },
+    resCatagory,
   };
-}
+};
