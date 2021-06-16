378243ab0deab58d9a2813427003816aa8101030
paiman <paimanrasoli789@gmail.com>
2021-06-16 13:38:15 +0430

    forget

--- components/Header.js
+++ components/Header.js
@@ -434,7 +434,7 @@ export default function Header({ categories, about }) {
       {/* end setting */}
       {subscribeModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-black max-w-myMaxWidth mx-auto">
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-gray-500 bg-opacity-30 max-w-myMaxWidth mx-auto">
             <div className="relative w-11/12 my-6 mx-auto max-w-3xl h-myHeight sm:h-myHeight md:h-auto">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
